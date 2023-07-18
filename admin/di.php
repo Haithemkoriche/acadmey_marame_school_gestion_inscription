facbc309c12237244f0d85fa48038bb49687fdda
Haithemkoriche <88111725+Haithemkoriche@users.noreply.github.com>
2023-07-18 20:12:55 +0100

    finalising

--- admin/di.php
+++ admin/di.php
@@ -173,26 +173,6 @@ WHERE di.id_ins = $id";
   </div>
 
  
-        <!-- Logout Modal -->
-        <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="logoutModalLabel" aria-hidden="true">
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="logoutModalLabel">Confirm Logout</h5>
-                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div class="modal-body">
-                <p>Are you sure you want to logout?</p>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                <a href="logout.php" class="btn btn-primary">Logout</a>
-              </div>
-            </div>
-          </div>
-        </div>
  <!-- Logout Modal -->
  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="logoutModalLabel" aria-hidden="true">
       <div class="modal-dialog" role="document">
--- admin/login.php
+++ admin/login.php
@@ -16,37 +16,37 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     exit;
   } else {
     // Authentification échouée, afficher un message d'erreur
-    $error = "Invalid username or password";
+    $error = "Nom d'utilisateur ou mot de passe incorrect";
   }
 }
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-  <title>Login</title>
+  <title>Connexion</title>
 </head>
 <body>
   <div class="container mt-5">
     <div class="row justify-content-center">
       <div class="col-md-6">
-        <h2 class="text-center mb-4">Admin Login</h2>
+        <h2 class="text-center mb-4">Connexion Administrateur</h2>
         <?php if (isset($error)) { ?>
           <div class="alert alert-danger"><?php echo $error; ?></div>
         <?php } ?>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
           <div class="form-group">
-            <label for="username">Username</label>
+            <label for="username">Nom d'utilisateur</label>
             <input type="text" class="form-control" id="username" name="username" required>
           </div>
           <div class="form-group">
-            <label for="password">Password</label>
+            <label for="password">Mot de passe</label>
             <input type="password" class="form-control" id="password" name="password" required>
           </div>
-          <button type="submit" class="btn btn-primary btn-block">Login</button>
+          <button type="submit" class="btn btn-primary btn-block">Connexion</button>
         </form>
       </div>
     </div>
